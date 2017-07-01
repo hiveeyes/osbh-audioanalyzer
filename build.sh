b0f058bede21aa0dc890fcde5cb99f57576dc909
Andreas Motl <github.amo@netfrag.org>
2017-07-01 03:01:45 +0200

    Improve build.sh for OSX

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 mkdir bin scriptname >/dev/null 2>&1
-g++ -std=c++11 main.cpp createFilters.cpp featureExtractor.cpp filter.cpp classifier.cpp -o bin/test
+
+GPP=g++
+
+# Check for modern compiler
+GPP_49=g++-mp-4.9
+if [ ! -z $(which $GPP_49) ]; then
+    GPP=$(which $GPP_49)
+fi
+
+$GPP -std=c++11 main.cpp createFilters.cpp featureExtractor.cpp filter.cpp classifier.cpp -o bin/test
